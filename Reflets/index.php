381128c2ed9151e58e1532aa0f6b10e8c0308078
Neamar <neamar@neamar.fr>
2017-08-10 17:39:42 -0700

    Names

--- Reflets/index.php
+++ Reflets/index.php
@@ -2,7 +2,7 @@
 //Infos relatives à la saga
 $_Nom='Reflets d\'Acide';
 $_Auteur='JBX';
-$NBEpisode=15;
+$NBEpisode=16;
 $_Prefix='Reflets';
 $PersoDefaut=array("NARRATEUR"=>"ABCDFF","ENORIEL"=>"ff0000","ZARAKAÏ"=>"00ff00","TRICHELIEU"=>"0000ff","WRANDRALL"=>"000000","ZEHIRMANN"=>"AAAAAA","MOUMOUNE"=>"FF22AA","ROGER"=>"AAFF22", "KYO"=>"22AAFF");
 $Bonus=array('Krak','Avril','BoitePerdue','BoitePerdue_2','Noel_d_Acide','Joyeux_Noel','Franconie','Fleau','Narrateur_Avril','Alkoor','Mordave','Cadeau_Acide','Cadeau_Accidentel',);
